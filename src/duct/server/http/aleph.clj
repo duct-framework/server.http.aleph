0ba82b9628ad74b941dfd274b977e96e05ab4702
James Reeves <jreeves@weavejester.com>
2017-06-05 21:24:54 +0100

    Add suspend and resume methods

--- src/duct/server/http/aleph.clj
+++ src/duct/server/http/aleph.clj
@@ -6,10 +6,25 @@
 (derive :duct.server.http/aleph :duct.server/http)
 
 (defmethod ig/init-key :duct.server.http/aleph [_ {:keys [handler logger] :as opts}]
-  (logger/log logger :report ::starting-server (select-keys opts [:port]))
-  {:server (aleph/start-server handler (dissoc opts :handler))
-   :logger logger})
+  (let [handler (atom (delay (:handler opts)))
+        logger  (atom logger)
+        options (dissoc opts :handler :logger)]
+    (logger/log @logger :report ::starting-server (select-keys opts [:port]))
+    {:handler handler
+     :logger  logger
+     :server  (aleph/start-server (fn [req] (@@handler req)) options)}))
 
 (defmethod ig/halt-key! :duct.server.http/aleph [_ {:keys [logger server]}]
-  (logger/log logger :report ::stopping-server)
+  (logger/log @logger :report ::stopping-server)
   (.close ^java.io.Closeable server))
+
+(defmethod ig/suspend-key! :duct.server.http/aleph [_ {:keys [handler]}]
+  (reset! handler (promise)))
+
+(defmethod ig/resume-key :duct.server.http/aleph [key opts old-opts old-impl]
+  (if (= (dissoc opts :handler :logger) (dissoc old-opts :handler :logger))
+    (do (deliver @(:handler old-impl) (:handler opts))
+        (reset! (:logger old-impl) (:logger opts))
+        old-impl)
+    (do (ig/halt-key! key old-impl)
+        (ig/init-key key opts))))
--- test/duct/server/http/aleph_test.clj
+++ test/duct/server/http/aleph_test.clj
@@ -42,3 +42,63 @@
         (ig/halt! system)
         (ig/halt! system)
         (is (thrown? ConnectException (http/get "http://127.0.0.1:3400/")))))))
+
+(deftest resume-and-suspend-test
+  (let [response1 {:status 200 :headers {} :body "foo"}
+        response2 {:status 200 :headers {} :body "bar"}
+        config1   {:duct.server.http/aleph {:port 3400, :handler (constantly response1)}}
+        config2   {:duct.server.http/aleph {:port 3400, :handler (constantly response2)}}]
+
+    (testing "suspend and resume"
+      (let [system1  (doto (ig/init config1) ig/suspend!)
+            response (future (http/get "http://127.0.0.1:3400/"))
+            system2  (ig/resume config2 system1)]
+        (try
+          (is (identical? (-> system1 :duct.server.http/aleph :handler)
+                          (-> system2 :duct.server.http/aleph :handler)))
+          (is (identical? (-> system1 :duct.server.http/aleph :server)
+                          (-> system2 :duct.server.http/aleph :server)))
+          (is (= (:status @response) 200))
+          (is (= (:body @response) "bar"))
+          (finally
+            (ig/halt! system1)
+            (ig/halt! system2)))))
+
+    (testing "suspend and resume with different config"
+      (let [system1  (doto (ig/init config1) ig/suspend!)
+            config2' (assoc-in config2 [:duct.server.http/aleph :port] 3401)
+            system2  (ig/resume config2' system1)]
+        (try
+          (let [response (http/get "http://127.0.0.1:3401/")]
+            (is (= (:status response) 200))
+            (is (= (:body response) "bar")))
+          (finally
+            (ig/halt! system1)
+            (ig/halt! system2)))))
+
+    (testing "suspend and resume with extra config"
+      (let [system1 (doto (ig/init {}) ig/suspend!)
+            system2 (ig/resume config2 system1)]
+        (try
+          (let [response (http/get "http://127.0.0.1:3400/")]
+            (is (= (:status response) 200))
+            (is (= (:body response) "bar")))
+          (finally
+            (ig/halt! system2)))))
+
+    (testing "suspend and result with missing config"
+      (let [system1  (doto (ig/init config1) ig/suspend!)
+            system2  (ig/resume {} system1)]
+        (is (= system2 {}))))
+
+    (testing "logger is replaced"
+      (let [logs1   (atom [])
+            logs2   (atom [])
+            config1 (assoc-in config1 [:duct.server.http/aleph :logger] (->TestLogger logs1))
+            config2 (assoc-in config2 [:duct.server.http/aleph :logger] (->TestLogger logs2))
+            system1 (doto (ig/init config1) ig/suspend!)
+            system2 (ig/resume config2 system1)]
+        (ig/halt! system2)
+        (is (= @logs1 [[::aleph/starting-server {:port 3400}]]))
+        (is (= @logs2 [[::aleph/stopping-server nil]]))
+        (ig/halt! system1)))))
