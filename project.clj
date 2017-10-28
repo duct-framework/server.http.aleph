430ef1dbeafb858be7fb03ed7200176f478cb9a5
James Reeves <jreeves@weavejester.com>
2017-10-29 00:51:45 +0100

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/server.http.aleph "0.1.1"
+(defproject duct/server.http.aleph "0.1.2"
   :description "Integrant methods for running an Aleph web server"
   :url "https://github.com/duct-framework/server.http.aleph"
   :license {:name "Eclipse Public License"
